f432b0c43e3327b3bb0658e7117938de8681d781
syzer3@gmail.com <syzer3@gmail.com>
2016-03-29 18:54:23 -0400

    reformat

--- fibonacci.py
+++ fibonacci.py
@@ -2,6 +2,7 @@ n = int(input())
 
 memo = {}
 
+
 def fib(n):
     if n <= 1:
         return 1
@@ -9,7 +10,8 @@ def fib(n):
     if n in memo:
         return memo[n]
 
-    memo[n] = fib(n-1) + fib(n-2)
+    memo[n] = fib(n - 1) + fib(n - 2)
     return memo[n]
+
 
 print(fib(n))
