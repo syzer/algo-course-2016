9bde0f6776345b64cf0e4cdfb11522a65dc43049
syzer3@gmail.com <syzer3@gmail.com>
2016-03-29 18:46:40 -0400

    memoized fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,15 @@
+n = int(input())
+
+memo = {}
+
 def fib(n):
     if n <= 1:
         return 1
-    else:
-        return fib(n-1) + fib(n-2)
 
-print(fib(10))
+    if n in memo:
+        return memo[n]
+
+    memo[n] = fib(n-1) + fib(n-2)
+    return memo[n]
+
+print(fib(n))
