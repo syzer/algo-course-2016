0685d34e44309dceb26a0a28490a08485b00dd55
syzer3@gmail.com <syzer3@gmail.com>
2016-03-31 16:55:55 -0400

    change take 1

--- greedy/change/change.py
+++ greedy/change/change.py
@@ -1,9 +1,20 @@
 # Uses python3
 import sys
 
+
 def get_change(n):
-    #write your code here
-    return n
+    coins = 0
+
+    while (n >= 10) and (n // 10):
+        n -= 10
+        coins += 1
+
+    while (n >= 5) and (n // 5):
+        n -= 5
+        coins += 1
+
+    return coins + n
+
 
 if __name__ == '__main__':
     n = int(sys.stdin.read())
