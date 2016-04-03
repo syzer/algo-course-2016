86cbe44ea903bfcf456f7cf69179e7aac4b9b8d6
syzer3@gmail.com <syzer3@gmail.com>
2016-04-03 10:03:52 -0400

    fractional kapsnack

--- greedy/fractional_knapsack/fractional_knapsack.py
+++ greedy/fractional_knapsack/fractional_knapsack.py
@@ -2,12 +2,23 @@
 # Uses python3
 import sys
 
+
 def get_optimal_value(capacity, weights, values):
     value = 0.
 
-    print(capacity, weights, values)
+    ratio = [x / y for x, y in zip(values, weights)]
+    all_items = list(zip(values, weights, ratio))
+    all_items.sort(key=lambda tup: -tup[2])
 
-    return value
+    for i in all_items:
+        left_load = capacity / i[1]
+        if left_load > 1:
+            left_load = 1
+        if left_load > 0:
+            capacity -= i[1]
+            value += i[0] * left_load
+
+    return round(value, 4)
 
 
 if __name__ == "__main__":
@@ -16,4 +27,4 @@ if __name__ == "__main__":
     values = data[2:(2 * n + 2):2]
     weights = data[3:(2 * n + 2):2]
     opt_value = get_optimal_value(capacity, weights, values)
-    print("{:.10f}".format(opt_value))
+    print("{:.4f}".format(opt_value))
