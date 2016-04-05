700a8ad7ba45be6fe497a2bfad215fe1caa8d662
syzer3@gmail.com <syzer3@gmail.com>
2016-04-05 17:11:03 -0400

    test differents summands

---
+++ greedy/different_summands/test_different_summands.py
@@ -0,0 +1,9 @@
+from different_summands import *
+
+assert (optimal_summands(6) == [1, 2, 3])
+assert (optimal_summands(8) == [1, 2, 5])
+assert (optimal_summands(10) == [1, 2, 3, 4])
+assert (optimal_summands(11) == [1, 2, 3, 5])
+assert (optimal_summands(13) == [1, 2, 3, 7])
+assert (optimal_summands(14) == [1, 2, 3, 8])
+assert (optimal_summands(15) == [1, 2, 3, 4, 5])
