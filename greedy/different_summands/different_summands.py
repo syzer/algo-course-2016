53a897a2a003a2e425afb7a0734d507491de3e24
syzer3@gmail.com <syzer3@gmail.com>
2016-04-05 17:11:24 -0400

    different summands implementation with while

--- greedy/different_summands/different_summands.py
+++ greedy/different_summands/different_summands.py
@@ -1,10 +1,20 @@
 # Uses python3
 import sys
 
+
 def optimal_summands(n):
     summands = []
-    #write your code here
+    i = 1
+    sum = 0
+
+    while (sum * 2 + 2) < n:
+        summands.append(i)
+        sum += i
+        i += 1
+
+    summands.append(n - sum)
     return summands
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
