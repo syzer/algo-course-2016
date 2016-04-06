b3c979678f7b313c0e8870cd9cf3622c7ebd3514
syzer3@gmail.com <syzer3@gmail.com>
2016-04-06 16:41:31 -0400

    stress test for binary search

--- divide-and-conquer/binary_search/test_binary_search.py
+++ divide-and-conquer/binary_search/test_binary_search.py
@@ -1,5 +1,7 @@
 from binary_search import *
 import random
+import sys
+
 
 def test_linear():
     a = [1, 5, 8, 12, 13]
@@ -9,6 +11,7 @@ def test_linear():
     assert linear_search(a, 1) == 0
     assert linear_search(a, 11) == -1
 
+
 def test_binary():
     a = [1, 5, 8, 12, 13]
     assert binary_search(a, 8) == 2
@@ -17,7 +20,38 @@ def test_binary():
     assert binary_search(a, 1) == 0
     assert binary_search(a, 11) == -1
 
+
 def test_stress_binary():
     a = list(range(0, 101))
     for i in range(200):
         assert binary_search(a, i) == linear_search(a, i)
+
+
+def stress():
+    MAX_EL = 1000
+    MAX_I = 20000
+
+    a = list(range(MAX_EL, 10000 + MAX_EL))
+    i = random.randint(1, MAX_I)
+
+    bin = binary_search(a, i)
+    lin = linear_search(a, i)
+    if bin != lin:
+        print(i)
+        print(a)
+
+    while bin == lin:
+        if i < 1500:
+            print(i, len(a))
+        a = list(range(MAX_EL, 10000 + MAX_EL))
+        i = random.randint(1, MAX_I)
+        bin = binary_search(a, i)
+        lin = linear_search(a, i)
+
+    print(i)
+    print(a)
+
+
+if __name__ == '__main__':
+    sys.setrecursionlimit(15000)
+    stress()
