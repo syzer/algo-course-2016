af9ac9bc70b28cfbac2c556e226442543a1a105c
syzer3@gmail.com <syzer3@gmail.com>
2016-04-06 16:27:59 -0400

    binary search 1

--- divide-and-conquer/binary_search/binary_search.py
+++ divide-and-conquer/binary_search/binary_search.py
@@ -1,9 +1,28 @@
 # Uses python3
 import sys
 
+
+def binary(a, left, right, x):
+    # NOT FOUND
+    if right < left:
+        # should be initial left - 1
+        return - 1
+
+    mid = left + (right - left) // 2
+
+    if a[mid] == x:
+        return mid
+    if x < a[mid]:
+        return binary(a, left, right - 1, x)
+    else:
+        return binary(a, left + 1, right, x)
+
+
+# adapter for binary
 def binary_search(a, x):
-    left, right = 0, len(a)
-    # write your code here
+    left, right = 0, len(a) - 1
+    return binary(a, left, right, x)
+
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -11,12 +30,12 @@ def linear_search(a, x):
             return i
     return -1
 
+
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n = data[0]
     m = data[n + 1]
-    a = data[1 : n + 1]
+    a = data[1: n + 1]
     for x in data[n + 2:]:
-        # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x), end=' ')
