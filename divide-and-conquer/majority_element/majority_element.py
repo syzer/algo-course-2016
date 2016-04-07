b549f464889cdb89ab60d8df62ed9ca6329ba66d
syzer3@gmail.com <syzer3@gmail.com>
2016-04-06 22:00:12 -0400

    pythonic version of solving majority element (using std lib)

--- divide-and-conquer/majority_element/majority_element.py
+++ divide-and-conquer/majority_element/majority_element.py
@@ -1,5 +1,6 @@
 # Uses python3
 import sys
+from collections import Counter
 
 
 def get_majority_element(a, left, right):
@@ -11,15 +12,18 @@ def get_majority_element(a, left, right):
     return -1
 
 
-def get_majority2(a, left, right):
-    return max(a, key=a.count)
-    # can also use Counter(d).most_common()[0]
+def get_majority_pythonic(a, left, n):
+    most_common = Counter(a).most_common()[0]
+    if most_common[1] > n / 2:
+        return 1
+    else:
+        return 0
 
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if get_majority2(a, 0, n) != -1:
+    if get_majority_pythonic(a, 0, n) != -1:
         print(1)
     else:
         print(0)
