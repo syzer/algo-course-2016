910dd7a2af479f29718a58828cd9160a446bba79
syzer3@gmail.com <syzer3@gmail.com>
2016-04-06 21:36:07 -0400

    fix majority element js

--- divide-and-conquer/majority_element/majority.element.js
+++ divide-and-conquer/majority_element/majority.element.js
@@ -1,9 +1,10 @@
-const arr = [2, 3, 9, 2, 2]
+const arr = [1, 3, 9, 2, 2]
+
 console.log(
     arr.sort((a, b) =>
         arr.reduce((l, r) =>
             l + (r == b) - (r == a)
-            , 0
+            , 1
         )
     )[0],
     arr
