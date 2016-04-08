c765b4a4319c2b70ea05dab9bcceae25ee64fdd0
syzer3@gmail.com <syzer3@gmail.com>
2016-04-08 12:24:14 -0400

    test count withpour comparition

---
+++ divide-and-conquer/test_sort_whitout_comparition.py
@@ -0,0 +1,12 @@
+# works for small int
+a = [2, 3, 2, 1, 3, 2, 2, 3, 2, 2, 2, 1]
+
+# 3 digits
+# left-> right
+# count apperances
+counts = [2, 7, 3]
+
+# we put 1 twice
+# 2 seven times
+# 3 three times
+b = [1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3]
