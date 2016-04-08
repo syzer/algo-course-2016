248a4ca77a5a9e032ffa15792658535fc0d24414
syzer3@gmail.com <syzer3@gmail.com>
2016-04-08 12:57:08 -0400

    count sort test

--- divide-and-conquer/test_sort_whitout_comparition.py
+++ divide-and-conquer/test_sort_whitout_comparition.py
@@ -1,3 +1,7 @@
+# nodemon -x 'py.test ./divide-and-conquer/test_sort_whitout_comparition.py'
+from count_sort import sort_count
+
+# Count sort
 # works for small int
 a = [2, 3, 2, 1, 3, 2, 2, 3, 2, 2, 2, 1]
 
@@ -10,3 +14,15 @@ counts = [2, 7, 3]
 # 2 seven times
 # 3 three times
 b = [1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3]
+
+
+def test_1():
+    assert sort_count(a) == b
+
+
+if __name__ == '__main__':
+    print('use pytest')
+
+
+# def most_common(lst):
+#     return max(set(lst), key=lst.count)
