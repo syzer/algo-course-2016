748901bd5a419f477c203bae6e4e0614eccde358
syzer3@gmail.com <syzer3@gmail.com>
2016-04-08 14:40:53 -0400

    testing majority element

--- divide-and-conquer/majority_element/test_majority_element.py
+++ divide-and-conquer/majority_element/test_majority_element.py
@@ -1,12 +1,16 @@
 # use py.test
 # nodemon -x 'py.test ./divide-and-conquer/majority_element/test_majority_element.py'from majority_element import *
-
+from majority_element import *
 
 def test_majority2():
     assert get_majority_pythonic([2, 3, 9, 2, 2], 0, 5) == 1
     assert get_majority_pythonic([1, 2, 3, 4], 0, 4) == 0
     assert get_majority_pythonic([1, 2, 3, 1], 0, 4) == 0
 
+def test_get_majority_element():
+    assert get_maj_el([2, 3, 9, 2, 2], 0, 5) == 1
+    assert get_maj_el([1, 2, 3, 4], 0, 4) == 0
+    assert get_maj_el([1, 2, 3, 1], 0, 4) == 0
 
 if __name__ == '__main__':
     print('Use py.test')
