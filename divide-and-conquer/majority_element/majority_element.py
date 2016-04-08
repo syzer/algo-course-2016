6e224f6481943ed30d2ecdb2772edb4f0ca67512
syzer3@gmail.com <syzer3@gmail.com>
2016-04-08 14:40:42 -0400

    debuging majority element

--- divide-and-conquer/majority_element/majority_element.py
+++ divide-and-conquer/majority_element/majority_element.py
@@ -8,8 +8,21 @@ def get_majority_element(a, left, right):
         return -1
     if left + 1 == right:
         return a[left]
-    # write your code here
+
+    mid = left + (right - left) // 2
+    print(a, left, right,get_majority_element(a, left, mid-1) )
+    if get_majority_element(a, left, mid - 1) == get_majority_element(a, mid + 1, right):
+        return 1
+
     return -1
+
+
+# proxy
+def get_maj_el(a, left, right):
+    if get_majority_element(a, left, right) == 1:
+        return 1
+    else:
+        return 0
 
 
 def get_majority_pythonic(a, left, n):
@@ -21,6 +34,7 @@ def get_majority_pythonic(a, left, n):
 
 
 if __name__ == '__main__':
+    sys.setrecursionlimit(15000)
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
     if get_majority_pythonic(a, 0, n) != -1:
