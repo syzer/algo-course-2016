7de1b9ec0e323bd1830a1961940d0f08683ff713
syzer3@gmail.com <syzer3@gmail.com>
2016-04-10 13:28:07 -0400

    test sorting, it modifies orginal array :(

--- divide-and-conquer/sorting/test_sorting.py
+++ divide-and-conquer/sorting/test_sorting.py
@@ -6,7 +6,14 @@ from .sorting import randomized_quick_sort, partition2, partition3
 
 def test_randomized_sort():
     a = [4, 3, 2, 5, 1]
-    assert randomized_quick_sort(a, 0, len(a) - 1) == sorted(a)
+    randomized_quick_sort(a, 0, len(a) - 1)  # :(
+    assert a == sorted(a)
+
+
+def test_randomized_sort_large():
+    a = [2, 3, 9, 2, 2, 3, 5, 6, 7, 8, 10, 11, 3, 1, 2, 9, 8, 7, 8, 6, 6, 3, 2]
+    randomized_quick_sort(a, 0, len(a) - 1)  # :(
+    assert a == sorted(a)
 
 
 if __name__ == '__main__':
