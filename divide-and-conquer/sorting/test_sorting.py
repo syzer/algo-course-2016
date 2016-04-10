35e31c315f9118c6da7988a4050b9020b2ebd7e0
syzer3@gmail.com <syzer3@gmail.com>
2016-04-10 12:32:19 -0400

    test sorting

---
+++ divide-and-conquer/sorting/test_sorting.py
@@ -0,0 +1,13 @@
+# Uses python3
+import sys
+import random
+from .sorting import randomized_quick_sort, partition2, partition3
+
+
+def test_randomized_sort():
+    a = [4, 3, 2, 5, 1]
+    assert randomized_quick_sort(a, 0, len(a) - 1) == sorted(a)
+
+
+if __name__ == '__main__':
+    print('use pytest')
