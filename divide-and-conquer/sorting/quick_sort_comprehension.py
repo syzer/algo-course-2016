95af2048d68682a35799461697763e38eb60c697
syzer3@gmail.com <syzer3@gmail.com>
2016-04-11 15:35:15 +0200

    qsort works with comprehension

---
+++ divide-and-conquer/sorting/quick_sort_comprehension.py
@@ -0,0 +1,15 @@
+import random
+
+
+def quick_sort(a):
+    if a == []:
+        return []
+    else:
+        pivot = a[0]
+        lesser = quick_sort([x for x in a[1:] if x < pivot])
+        greater = quick_sort([x for x in a[1:] if x >= pivot])
+        return lesser + [pivot] + greater
+
+
+a = [int(1000 * random.random()) for i in range(10000)]
+print(qsort1(a))
