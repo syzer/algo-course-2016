a43b4570569bda4b28232407b5d521373435a07c
syzer3@gmail.com <syzer3@gmail.com>
2016-04-11 17:19:19 +0200

    all tests passes again

--- divide-and-conquer/sorting/sorting.py
+++ divide-and-conquer/sorting/sorting.py
@@ -3,37 +3,33 @@ import sys
 import random
 
 
-def part3(a, left, right):
-    pivot = random.randint(left, right)
-    a[left], a[pivot] = a[pivot], a[left]
-    i = left + 1
-    pivot = a[left]
+def randomized_quick_sort3(a, left, right):
+    if right - left < 2: return
+    key = a[random.randrange(left, right)]
+    i = left
+    j = left
+    pivot = right
 
-    for j in range(left + 1, right + 1):
-        if a[j] < pivot:
+    while j < pivot:
+        if a[j] < key:
             a[j], a[i] = a[i], a[j]
             i += 1
+            j += 1
+        elif a[j] == key:
+            j += 1
+        else:
+            pivot -= 1
+            a[j], a[pivot] = a[pivot], a[j]
 
-    pos = i - 1
-    a[left], a[pos] = a[pos], a[left]
-
-    return pos
-
-
-# 3-way partition
-def randomized_quick_sort3(a, l, r):
-    if l >= r:
-        return
-
-    m = part3(a, l, r)
-    randomized_quick_sort3(a, l, m - 1)
-    randomized_quick_sort3(a, m + 1, r)
+    randomized_quick_sort3(a, left, i)
+    randomized_quick_sort3(a, pivot, right)
 
 
 #     :( modifies array
 if __name__ == '__main__':
+    sys.setrecursionlimit(15000)
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    randomized_quick_sort3(a, 0, n - 1)
+    randomized_quick_sort3(a, 0, n)
     for x in a:
         print(x, end=' ')
--- divide-and-conquer/sorting/test_sorting.py
+++ divide-and-conquer/sorting/test_sorting.py
@@ -3,45 +3,49 @@ import random
 from .sorting import *
 
 random.seed(10)
-
+sys.setrecursionlimit(15000)
 
 def test_randomized_sort():
     a = [4, 3, 2, 5, 1]
-    randomized_quick_sort3(a, 0, len(a) - 1)
+    randomized_quick_sort3(a, 0, len(a))
     assert a == sorted(a)
 
 
 def test_randomized_sort_large():
     a = [2, 3, 9, 2, 2, 3, 5, 6, 7, 8, 10, 11, 3, 1, 2, 9, 8, 7, 8, 6, 6, 3, 2]
-    randomized_quick_sort3(a, 0, len(a) - 1)  # :(
+    randomized_quick_sort3(a, 0, len(a))
     assert a == sorted(a)
 
 
 def test_on_very_large_random_list():
     b = [i for i in range(2000)]
     random.shuffle(b)
-    randomized_quick_sort3(b, 0, len(b) - 1)  # :(
+    randomized_quick_sort3(b, 0, len(b))
     assert b == sorted(b)
 
 
 def test_on_weird_input():
     b = [100, 1]
     random.shuffle(b)
-    randomized_quick_sort3(b, 0, len(b) - 1)  # :(
+    randomized_quick_sort3(b, 0, len(b))
     assert b == sorted(b)
 
 
 def test_on_random_list():
     a = [int(1000 * random.random()) for i in range(10000)]
-    randomized_quick_sort3(a, 0, len(a) - 1)  # :(
+    randomized_quick_sort3(a, 0, len(a))
+    assert a == sorted(a)
+
+def test_on_large_equal_list():
+    a = [int(5 * random.random()) for i in range(10000)]
+    randomized_quick_sort3(a, 0, len(a))
     assert a == sorted(a)
 
 
 def test_on_same_input():
     a = [2, 2, 2, 5, 5, 5, 5, 3, 3, 3, 3, 3, 3, 3]
-    randomized_quick_sort3(a, 0, len(a) - 1)  # :(
+    randomized_quick_sort3(a, 0, len(a))
     assert a == sorted(a)
-
 
 if __name__ == '__main__':
     print('use pytest')
