59fb9510fd3ff4b8aa023fbe9e2e718a1e5c9520
syzer3@gmail.com <syzer3@gmail.com>
2016-04-16 13:27:46 +0200

    more performant merge sort implementation

--- divide-and-conquer/sorting/merge_sort.py
+++ divide-and-conquer/sorting/merge_sort.py
@@ -31,6 +31,50 @@ def merge(left, right):
     return output
 
 
+def merge_sort2(x):
+    result = []
+    if len(x) < 2:
+        return x
+    mid = int(len(x)/2)
+    y = merge_sort2(x[:mid])
+    z = merge_sort2(x[mid:])
+    i = 0
+    j = 0
+    while i < len(y) and j < len(z):
+        if y[i] > z[j]:
+            result.append(z[j])
+            j += 1
+        else:
+            result.append(y[i])
+            i += 1
+    result += y[i:]
+    result += z[j:]
+
+    return result
+
+
+def merge_sort3(x):
+    result = []
+    if len(x) < 20:
+        return sorted(x)
+    mid = int(len(x) / 2)
+    y = merge_sort3(x[:mid])
+    z = merge_sort3(x[mid:])
+    i = 0
+    j = 0
+    while i < len(y) and j < len(z):
+        if y[i] > z[j]:
+            result.append(z[j])
+            j += 1
+        else:
+            result.append(y[i])
+            i += 1
+    result += y[i:]
+    result += z[j:]
+
+    return result
+
+
 a = [7, 2, 5, 3, 7, 13, 1, 6]
 test = merge_sort(a)
 print(test)
@@ -38,3 +82,11 @@ print(test)
 
 def test_1():
     assert test == sorted(a)
+
+def test_2():
+    a = [7, 2, 5, 3, 7, 13, 1, 6]
+    assert merge_sort2(a) == sorted(a)
+
+def test_3():
+    a = [7, 2, 5, 3, 7, 13, 1, 6]
+    assert merge_sort3(a) == sorted(a)
