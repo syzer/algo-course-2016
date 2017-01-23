f2fb96506769460484a056b1a9a2af6401555c51
syzer3@gmail.com <syzer3@gmail.com>
2017-01-23 22:19:15 +0100

    anagrams

--- recursion/anagrams_using_hashtable.py
+++ recursion/anagrams_using_hashtable.py
@@ -3,10 +3,8 @@ from hash_table import HashTableDict
 
 # f O(N L log L)
 def find_anagrams(words):
-    # FUNCTION find_anagrams(words)
     hash_dict = HashTableDict()
 
-    # hash.get_or_default(sort(word), []).push(word)
     for word in words:
         hash_dict.add(word)
 
